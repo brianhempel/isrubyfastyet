caba76e2bc5318a406a0ae58efbcb4692f8b2928
Brian Hempel <bnhempel@mtu.edu>
2012-01-30 11:39:09 -0500

    Initialize front-end web app

---
+++ isrubyfastyet.com/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IsrubyfastyetCom::Application.config.secret_token = 'd0ec7557f86afde68bd08a3e466b86a0b92c16c9f970c62f3dfcf32ba689ade987e48c37cb31f608927adf77f3afbea7a6e7ba782aaea47a9db2785f6cdfe1d7'

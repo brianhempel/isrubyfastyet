311367b5937d7840171dda9fe1fd7dfb60a52164
Brian Hempel <bnhempel@mtu.edu>
2012-02-13 15:45:11 -0500

    Better failure message for JSON mismatch

--- isrubyfastyet.com/spec/support/json_matchers.rb
+++ isrubyfastyet.com/spec/support/json_matchers.rb
@@ -2,4 +2,7 @@ RSpec::Matchers.define :be_json_like do |expected|
   match do |actual|
     JSON.parse(actual) == JSON.parse(expected)
   end
+  failure_message_for_should do |actual|
+    "expected #{actual} to be json like #{expected}"
+  end
 end

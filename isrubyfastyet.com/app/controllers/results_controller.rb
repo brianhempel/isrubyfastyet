a859e2b416b79ee6d79925703ad3bf26f9cb499c
Brian Hempel <bnhempel@mtu.edu>
2012-08-13 00:35:03 -0400

    Results json can be filtered by start_time

--- isrubyfastyet.com/app/controllers/results_controller.rb
+++ isrubyfastyet.com/app/controllers/results_controller.rb
@@ -3,6 +3,18 @@ class ResultsController < ApplicationController
 
   def index
     @benchmark = RubyBenchmark.from_param(params[:benchmark_id])
-    @results = @benchmark.passing_results
+    @results = filter_by_params(@benchmark.passing_results, params)
+  end
+
+  private
+
+  def filter_by_params(results, params)
+    results.select do |result|
+      if params[:start_time].present?
+        Time.zone.parse(params[:start_time]).utc <= result.time
+      else
+        true
+      end
+    end
   end
 end
--- isrubyfastyet.com/spec/requests/results_controller_spec.rb
+++ isrubyfastyet.com/spec/requests/results_controller_spec.rb
@@ -11,6 +11,12 @@ describe "/benchmarks/benchmark_param/results" do
   before do
     # remove result files
     Dir.glob(File.join(@results_dir, '*')).each { |path| File.unlink(path) }
+
+    create_result_file("benchmark_1_results.tsv", <<-RESULTS)
+2012-01-12 07:23:49 UTC	1.8.7	56.6861324310303 MB	ruby 1.8.7 (2011-06-30 patchlevel 352) [i686-darwin10.8.0]
+2012-01-12 07:23:49 UTC	1.8.7	failed	ruby 1.8.7 (2011-06-30 patchlevel 352) [i686-darwin10.8.0]
+2012-01-12 08:55:44 UTC	jruby-head	208.656998634338 MB	jruby 1.7.0.dev (ruby-1.8.7-p357) (2012-01-12 0e83d96) (Java HotSpot(TM) 64-Bit Server VM 1.6.0_29) [darwin-x86_64-java]
+    RESULTS
   end
 
   def create_result_file(name, data)
@@ -21,12 +27,6 @@ describe "/benchmarks/benchmark_param/results" do
 
   describe ".json" do
     it "returns all the results" do
-      create_result_file("benchmark_1_results.tsv", <<-RESULTS)
-2012-01-12 07:23:49 UTC	1.8.7	56.6861324310303 MB	ruby 1.8.7 (2011-06-30 patchlevel 352) [i686-darwin10.8.0]
-2012-01-12 07:23:49 UTC	1.8.7	failed	ruby 1.8.7 (2011-06-30 patchlevel 352) [i686-darwin10.8.0]
-2012-01-12 08:55:44 UTC	jruby-head	208.656998634338 MB	jruby 1.7.0.dev (ruby-1.8.7-p357) (2012-01-12 0e83d96) (Java HotSpot(TM) 64-Bit Server VM 1.6.0_29) [darwin-x86_64-java]
-      RESULTS
-
       visit "/benchmarks/benchmark_1/results.json"
 
       page.source.should be_json_like(<<-JSON)
@@ -50,6 +50,47 @@ describe "/benchmarks/benchmark_param/results" do
               "full_version": "jruby 1.7.0.dev (ruby-1.8.7-p357) (2012-01-12 0e83d96) (Java HotSpot(TM) 64-Bit Server VM 1.6.0_29) [darwin-x86_64-java]"
             }
           ]
+        }
+      JSON
+    end
+  end
+
+  describe ".json?start_time=2012-01-12+07:23:50+UTC" do
+    it "returns only the results after the start time" do
+      visit "/benchmarks/benchmark_1/results.json?start_time=2012-01-12+07:23:50+UTC"
+
+      page.source.should be_json_like(<<-JSON)
+        {
+          "benchmark": {
+            "name":  "Benchmark 1",
+            "param": "benchmark_1",
+            "units": "MB"
+          },
+          "results": [
+            {
+              "time_str":     "2012-01-12 08:55:44 UTC",
+              "rvm_name":     "jruby-head",
+              "result":       208.656998634338,
+              "full_version": "jruby 1.7.0.dev (ruby-1.8.7-p357) (2012-01-12 0e83d96) (Java HotSpot(TM) 64-Bit Server VM 1.6.0_29) [darwin-x86_64-java]"
+            }
+          ]
+        }
+      JSON
+    end
+  end
+
+  describe ".json?start_time=2012-01-13" do
+    it "returns only the results after the start time" do
+      visit "/benchmarks/benchmark_1/results.json?start_time=2012-01-13"
+
+      page.source.should be_json_like(<<-JSON)
+        {
+          "benchmark": {
+            "name":  "Benchmark 1",
+            "param": "benchmark_1",
+            "units": "MB"
+          },
+          "results": []
         }
       JSON
     end

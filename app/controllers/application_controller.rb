16cadd30ced31645e860a6419a4f243052375195
Brian Hempel <bnhempel@mtu.edu>
2012-09-06 00:02:15 -0400

    Try to make the app more fast

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  before_filter { headers['Cache-Control'] = "public, max-age=900" } if Rails.env.production?
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,9 +7,10 @@ IsRubyFastYet::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.static_cache_control              = "public, max-age=2592000"
 
-  # Disable Rails's static asset server (Apache or nginx will already do this)
-  config.serve_static_assets = false
+  # Enable Rails's static asset server (Apache or nginx will already do this)
+  config.serve_static_assets = true
 
   # Compress JavaScripts and CSS
   config.assets.compress = true

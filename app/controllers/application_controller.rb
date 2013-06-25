c353e8e80708ada0cac29027b5372455eb60617d
Brian Hempel <bnhempel@mtu.edu>
2013-06-25 15:51:28 -0400

    Adjust HTTP caching to cache until the next benchmark run (1 day)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter { headers['Cache-Control'] = "public, max-age=900" } if Rails.env.production?
+  before_filter { headers['Cache-Control'] = 'public'; headers['Expires'] = 1.day.from_now.httpdate } if Rails.env.production?
 end
